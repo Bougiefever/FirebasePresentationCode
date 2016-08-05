5f27e9cbcbd8afc9c281901c216e72eb708e3e8d
Anne Bougie <anne.bougie@gmail.com>
2016-08-05 18:02:35 -0500

    Android and web start

---
+++ android/MyShoppingList/app/src/main/java/com/bougie/annie/myshoppinglist/ShoppingItem.java
@@ -0,0 +1,17 @@
+package com.bougie.annie.myshoppinglist;
+
+/**
+ * Created by annie on 8/5/16.
+ */
+public class ShoppingItem {
+    public String item;
+    public String name;
+
+    public ShoppingItem() {
+    }
+
+    public ShoppingItem(String name, String item) {
+        this.name = name;
+        this.item = item;
+    }
+}
