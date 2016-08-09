d5c4a9b6a4e68fb7931489b21adac7f3200772eb
Anne Bougie <anne.bougie@gmail.com>
2016-08-09 11:50:00 -0500

    TC Sample working - new starting point

--- android/ThatConferenceShopping/app/src/main/java/com/anniebougie/thatconferenceshopping/MainActivity.java
+++ android/ThatConferenceShopping/app/src/main/java/com/anniebougie/thatconferenceshopping/MainActivity.java
@@ -21,19 +21,13 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.appinvite.AppInviteInvitation;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.crash.FirebaseCrash;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -45,8 +39,6 @@ import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
         implements GoogleApiClient.OnConnectionFailedListener {
@@ -88,7 +80,6 @@ public class MainActivity extends AppCompatActivity
         _firebaseAuth = FirebaseAuth.getInstance();
         _firebaseUser = _firebaseAuth.getCurrentUser();
 
-
         if (_firebaseUser == null) {
             // Not signed in, launch the Sign In activity
             startActivity(new Intent(this, SigninActivity.class));
@@ -106,38 +97,6 @@ public class MainActivity extends AppCompatActivity
                 .addApi(Auth.GOOGLE_SIGN_IN_API)
                 .build();
 
-        _firebaseDatabase = FirebaseDatabase.getInstance();
-        _firebaseDatabaseReference = _firebaseDatabase.getReference();
-
-        _childEventListener = new  ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                ShoppingItem shoppingItem = dataSnapshot.getValue(ShoppingItem.class);
-                addNewShoppingListItem(shoppingItem.item);
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-                ShoppingItem shoppingItem = dataSnapshot.getValue(ShoppingItem.class);
-                removeShoppingListItem(shoppingItem.item);
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        };
-
         final ListView listView = (ListView) findViewById(R.id.listv);
         String[] items = {};
         _arrayList = new ArrayList(Arrays.asList(items));
@@ -154,26 +113,8 @@ public class MainActivity extends AppCompatActivity
                 adb.setNegativeButton("No", null);
                 adb.setPositiveButton("Yes", new AlertDialog.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        //removeShoppingListItem(itemText);
-                    _firebaseDatabaseReference.child("items")
-                            .orderByChild("item")
-                            .equalTo(itemText)
-                            .addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot dataSnapshot) {
-                                    for (DataSnapshot child : dataSnapshot.getChildren()) {
-                                        String key = "items/" + child.getKey();
-                                        DatabaseReference removeIt = _firebaseDatabase
-                                                .getReference(key);
-                                        removeIt.removeValue();
-                                    }
-                                }
+                        removeShoppingListItem(itemText);
 
-                                @Override
-                                public void onCancelled(DatabaseError databaseError) {
-
-                                }
-                            });
                     }
                 });
                 adb.show();
@@ -186,29 +127,13 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 String newItem = _inputItem.getText().toString();
-                //addNewShoppingListItem(newItem);
-                ShoppingItem shoppingItem = new ShoppingItem(_firebaseUser.getDisplayName(),
-                        newItem);
-                _firebaseDatabaseReference.child("items").push().setValue(shoppingItem);
+                addNewShoppingListItem(newItem);
+
                 _inputItem.setText("");
             }
         });
 
-        _firebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
-        _firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
-        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
-                .setDeveloperModeEnabled(BuildConfig.DEBUG)
-                .build();
-        Map<String, Object> defaultConfigMap = new HashMap<>();
-        defaultConfigMap.put("number_of_items", 3L);
-        _firebaseRemoteConfig.setDefaults(R.xml.remote_config_defaults);
-        listenForShoppingItemsChanges();
-
-        MobileAds.initialize(getApplicationContext(), String.valueOf(R.string.banner_ad_unit_id));
-        _adView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        _adView.loadAd(adRequest);
     }
 
     private void addNewShoppingListItem(String item) {
@@ -262,25 +187,8 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-    private void sendInvitation(){
-        Intent intent = new AppInviteInvitation.IntentBuilder(getString(R.string.invitation_title))
-                .setMessage(getString(R.string.invitation_message))
-                .setCallToActionText(getString(R.string.invitation_cta))
-                .build();
-        startActivityForResult(intent, REQUEST_INVITE);
-    }
+    private void sendInvitation() {
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == REQUEST_INVITE) {
-            if (resultCode == RESULT_OK) {
-                Bundle payload = new Bundle();
-                payload.putString(FirebaseAnalytics.Param.VALUE, "invitation_sent");
-                _firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SHARE, payload);
-            }
-        }
     }
 
     private void crashApp() {
@@ -288,30 +196,15 @@ public class MainActivity extends AppCompatActivity
             throw new RuntimeException("Testing crash reporting");
         } catch (Exception exception) {
             //todo crash reporting to Firebase
-            FirebaseCrash.log("Report about a crach event");
-            FirebaseCrash.report(exception);
             throw exception;
         }
     }
 
     private void fetchConfigValues() {
-        _firebaseRemoteConfig.fetch()
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            _firebaseRemoteConfig.activateFetched();
-                            listenForShoppingItemsChanges();
-                        }
-                    }
-                });
+
     }
 
     private void listenForShoppingItemsChanges() {
-        int num = (int) _firebaseRemoteConfig.getLong("number_of_items");
-        _arrayList.clear();
-        _firebaseDatabaseReference.child("items")
-                .limitToFirst(num)
-                .addChildEventListener(_childEventListener);
+
     }
 }
---
+++ android/ThatConferenceShopping/app/src/main/java/com/anniebougie/thatconferenceshopping/ShoppingItem.java
@@ -0,0 +1,17 @@
+package com.anniebougie.thatconferenceshopping;
+
+/**
+ * Created by annie on 8/9/16.
+ */
+public class ShoppingItem {
+    public String item;
+    public String name;
+
+    public ShoppingItem() {
+    }
+
+    public ShoppingItem(String name, String item) {
+        this.name = name;
+        this.item = item;
+    }
+}
